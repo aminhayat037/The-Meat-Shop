de4b59620ae94fd8b6cfc9f9b9cf67c24cfbed15
aminhayat037 <aminhayat037@gmail.com>
2026-06-29 14:34:03 +0100

    Create product.js

---
+++ product.js
@@ -0,0 +1,26 @@
+const products = [
+{
+name: "Ribeye Steak",
+price: 12.99,
+category: "steak",
+image: "https://images.unsplash.com/photo-1600891964599-f61ba0e24092"
+},
+{
+name: "Chicken Breast",
+price: 5.99,
+category: "chicken",
+image: "https://images.unsplash.com/photo-1604908176997-125f25cc6f3d"
+},
+{
+name: "Lamb Chops",
+price: 10.99,
+category: "lamb",
+image: "https://images.unsplash.com/photo-1617191519005-d4b4c0d4a4c2"
+},
+{
+name: "Qurbani Whole Lamb",
+price: 199.99,
+category: "qurbani",
+image: "https://images.unsplash.com/photo-1600891963935-c6e5b9a87c0f"
+}
+];
